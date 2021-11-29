1a837d150d65877397f1ca30cd10121808e598d7
Victor <trannvictor@gmail.com>
2021-11-29 15:12:10 -0800

    Created LichessTester class

--- lichess_driver/main.py
+++ lichess_driver/main.py
@@ -9,9 +9,10 @@ from selenium.webdriver.support import expected_conditions as EC
 import time  # Allows us to sleep for a certain number of seconds
 
 
-
+"""
 # Constants
 LICHESS_URL = "https://lichess.org"
+#CHROMEDRIVER_PATH = "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe"
 CHROMEDRIVER_PATH = "E:\\chromedriver\\chromedriver.exe"
 SERVICE = Service(CHROMEDRIVER_PATH)
 MAX_WAIT_FOR_SECONDS = 10
@@ -24,8 +25,12 @@ XPATH_PUZZLES_STORM = "//a[@href='/storm']"
 XPATH_PUZZLES_RACER = "//a[@href='/racer']"
 XPATH_SEARCH_BAR = "//header[@id='top']//div[@class='site-buttons']//div[@id='clinput']//a[@class='link']" \
     # specify the element by multiple identifiers, separated by '//'
+"""
 
 
+CHROMEDRIVER_PATH = "E:\\chromedriver\\chromedriver.exe"
+SERVICE = Service(CHROMEDRIVER_PATH)
+MAX_WAIT_FOR_SECONDS = 10
 
 class WebTester:
     driver = None
@@ -33,14 +38,17 @@ class WebTester:
 
     def __init__(self):
         self.driver = webdriver.Chrome(service=SERVICE)
+        self.chrome_window_maximize()
         self.action = ActionChains(self.driver)
 
     def __del__(self):
         self.driver.close()
 
-    def open_website(self, url):
-        """ Open a website given a URL """
-        self.driver.get(LICHESS_URL)
+
+    def chrome_window_maximize(self):
+        """ Maximize the chrome window """
+        self.driver.maximize_window()
+
 
     def hover(self, xpath):
         """ Hover over an element based on a given xpath """
@@ -48,9 +56,6 @@ class WebTester:
         self.action.move_to_element(element)
         self.action.perform()
 
-    def hover_puzzles(self):
-        """ Hover over the Puzzles tab on the top bar menu """
-        self.hover(XPATH_PUZZLES)
 
     def click(self, xpath):
         """ Click on an element based on a given xpath """
@@ -59,41 +64,6 @@ class WebTester:
         self.action.click()
         self.action.perform()
 
-    def click_puzzles(self):
-        """ Click on the Puzzles tab on the top bar menu """
-        self.click(XPATH_PUZZLES)
-
-    def click_puzzles_dashboard(self):
-        """ Click on the Puzzles Dashboard button under the Puzzles tab """
-        self.hover_puzzles()
-        self.click(XPATH_PUZZLES_DASHBOARD)
-
-    def click_puzzles_streak(self):
-        """ Click on the Puzzles Streak button under the Puzzles tab """
-        self.hover_puzzles()
-        self.click(XPATH_PUZZLES_STREAK)
-
-    def click_puzzles_storm(self):
-        """ Click on the Puzzles Storm button under the Puzzles tab """
-        self.hover_puzzles()
-        self.click(XPATH_PUZZLES_STORM)
-
-    def click_puzzles_racer(self):
-        """ Click on the Puzzles Racer button under the Puzzles tab """
-        self.hover_puzzles()
-        self.click(XPATH_PUZZLES_RACER)
-
-    def search(self, string_input):
-        """ Search given an input """
-        search_bar = self.driver.find_element(By.XPATH, XPATH_SEARCH_BAR)
-        self.action.move_to_element(search_bar)
-        self.action.click()
-        self.action.send_keys(string_input)
-        self.action.perform()
-        time.sleep(1)   # delay so we can see the input on the search bar
-        self.action.send_keys(Keys.RETURN)
-        self.action.perform()
-        # self.action.move_to_element(element).click().send_keys(input).send_keys(Keys.RETURN).perform() # one liner
 
     def wait_for(self, xpath):
         """ Waits for an element, given by its xpath, to appear on the page before proceeding """
@@ -106,23 +76,87 @@ class WebTester:
             self.driver.quit()
 
 
-if __name__ == '__main__':
-    web_tester = WebTester()
-    web_tester.open_website(LICHESS_URL)
-    time.sleep(2)
-    web_tester.click_puzzles()
-    time.sleep(2)
-    web_tester.click_puzzles_dashboard()
-    time.sleep(2)
-    web_tester.click_puzzles_streak()
-    time.sleep(2)
-    web_tester.click_puzzles_storm()
-    time.sleep(2)
-    web_tester.click_puzzles_racer()
-    time.sleep(2)
-    web_tester.search("Hello, world!")
-    time.sleep(10)
 
+class LichessTester(WebTester):
+    url = "https://lichess.org"
+    xpath = {
+        "puzzles"       : "//a[@href='/training']",
+        "dashboard"     : "//a[@href='/training/dashboard/30']",
+        "streak"        : "//a[@href='/streak']",
+        "storm"         : "//a[@href='/storm']",
+        "racer"         : "//a[@href='/racer']",
+        "search_bar"    : "//header[@id='top']//div[@class='site-buttons']//div[@id='clinput']//a[@class='link']" \
+        # specify the element by multiple identifiers, separated by '//'
+    }
+
+    def __init__(self):
+        super().__init__()
+
+    def open_website(self):
+        """ Open a website given a URL """
+        self.driver.get(self.url)
+
+    def hover_puzzles(self):
+        """ Hover over the Puzzles tab on the top bar menu """
+        self.hover(self.xpath.get("puzzles"))
+
+    def click_puzzles(self):
+        """ Click on the Puzzles tab on the top bar menu """
+        self.click(self.xpath.get("puzzles"))
+
+    def click_puzzles_dashboard(self):
+        """ Click on the Puzzles Dashboard button under the Puzzles tab """
+        self.hover_puzzles()
+        self.click(self.xpath.get("streak"))
+
+    def click_puzzles_streak(self):
+        """ Click on the Puzzles Streak button under the Puzzles tab """
+        self.hover_puzzles()
+        self.click(self.xpath.get("streak"))
+
+    def click_puzzles_storm(self):
+        """ Click on the Puzzles Storm button under the Puzzles tab """
+        self.hover_puzzles()
+        self.click(self.xpath.get("storm"))
+
+    def click_puzzles_racer(self):
+        """ Click on the Puzzles Racer button under the Puzzles tab """
+        self.hover_puzzles()
+        self.click(self.xpath.get("racer"))
+
+    def search(self, string_input):
+        """ Search given an input """
+        search_bar = self.driver.find_element(By.XPATH, self.xpath.get("search_bar"))
+        self.action.move_to_element(search_bar)
+        self.action.click()
+        self.action.send_keys(string_input)
+        self.action.perform()
+        time.sleep(1)   # delay so we can see the input on the search bar
+        self.action.send_keys(Keys.RETURN)
+        self.action.perform()
+        # self.action.move_to_element(element).click().send_keys(input).send_keys(Keys.RETURN).perform() # one liner
+
+
+
+
+
+
+if __name__ == '__main__':
+    lichess_tester = LichessTester()
+    lichess_tester.open_website()
+    time.sleep(2)
+    lichess_tester.click_puzzles()
+    time.sleep(2)
+    lichess_tester.click_puzzles_dashboard()
+    time.sleep(2)
+    lichess_tester.click_puzzles_streak()
+    time.sleep(2)
+    lichess_tester.click_puzzles_storm()
+    time.sleep(2)
+    lichess_tester.click_puzzles_racer()
+    time.sleep(2)
+    lichess_tester.search("Hello, world!")
+    time.sleep(10)
 
 """
 SUBMENU CLICK
